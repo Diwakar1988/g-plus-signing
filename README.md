f1a19546e0f483acee8a48f7659872994c571500
Diwakar Mishra <diwakar.mishra@3pillarglobal.com>
2016-03-16 18:56:30 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple demo to explain google & google plus sign-in 
---
+++ app/src/androidTest/java/com/github/diwakar1988/gsigning/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.diwakar1988.gsigning;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
